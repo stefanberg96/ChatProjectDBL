f5447f50e06ee5f7607289ea815a9fe8b8698867
bergshm <bergshm@gmail.com>
2016-03-07 15:05:26 +0100

    removed floating button

--- app/src/main/java/dbl/tue/chatproject/ChatProject.java
+++ app/src/main/java/dbl/tue/chatproject/ChatProject.java
@@ -31,7 +31,7 @@ public class ChatProject extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_chat_project, menu);
+        //getMenuInflater().inflate(R.menu.menu_chat_project, menu);
         return true;
     }
 
