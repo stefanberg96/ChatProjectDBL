24b0f9d4dfefbb3a5d0d7858a1e6c7a771d19c97
bergshm <bergshm@gmail.com>
2016-03-07 15:19:08 +0100

    created user class

---
+++ app/src/main/java/User.java
@@ -0,0 +1,71 @@
+/**
+ * User class
+ */
+public class User {
+
+    int UserID;
+    String firstName;
+    String lastName;
+    String facebook_Usertoken;
+    int facebook_UserID;
+    String profilePictureURL;
+    String bio;
+
+
+    public int getUserID() {
+        return UserID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFacebook_Usertoken() {
+        return facebook_Usertoken;
+    }
+
+    public void setFacebook_Usertoken(String facebook_Usertoken) {
+        this.facebook_Usertoken = facebook_Usertoken;
+    }
+
+    public int getFacebook_UserID() {
+        return facebook_UserID;
+    }
+
+    public void setFacebook_UserID(int facebook_UserID) {
+        this.facebook_UserID = facebook_UserID;
+    }
+
+    public String getProfilePictureURL() {
+        return profilePictureURL;
+    }
+
+    public void setProfilePictureURL(String profilePictureURL) {
+        this.profilePictureURL = profilePictureURL;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+
+
+
+
+}
