99d4a25db46a8c4cf7c0f7807681c3e6c800a78f
bergshm <bergshm@gmail.com>
2016-03-07 15:23:49 +0100

    created message class

---
+++ app/src/main/java/Message.java
@@ -0,0 +1,46 @@
+/**
+ * Message class
+ */
+public class Message {
+    String data;
+    User recipient;
+    User sender;
+    double timestamp;
+
+    Message(String data){
+        this.data=data;
+        timestamp=System.currentTimeMillis();
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public double getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(double timestamp) {
+        this.timestamp = timestamp;
+    }
+}
